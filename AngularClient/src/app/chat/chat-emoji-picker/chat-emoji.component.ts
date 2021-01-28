550da9cfad133a7d3cd5cd81a729c7999960dd19
Christopher Buchberger <c.buchberger01@gmail.com>
2021-01-28 01:03:39 +0100

    update

---
+++ AngularClient/src/app/chat/chat-emoji-picker/chat-emoji.component.ts
@@ -0,0 +1,41 @@
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Subscription} from "rxjs";
+import {ChatService} from "../chat.service";
+import {MessageControlService} from "../../core/services/message-control.service";
+import {ChatModel} from "../../core/models/chat";
+
+@Component({
+  selector: 'emoji-picker',
+  templateUrl: './chat-emoji.component.html',
+  styleUrls: ['./chat-emoji.component.scss']
+})
+export class ChatDetailComponent implements OnInit, OnDestroy {
+  @ViewChild("scrollable", {static: false}) scrollable;
+  subscription: Subscription;
+  chat: ChatModel;
+  sending: boolean = false;
+
+  constructor(private service: ChatService, private control: MessageControlService) {
+  }
+
+  ngOnInit() {
+
+  }
+
+  scrollToBottom() {
+
+  }
+
+  saveMessage($event) {
+
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+
+  emojiPickerDialog(){
+
+  }
+}
